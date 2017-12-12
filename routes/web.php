03d0ebc12627d2950566280b91ae19ade97a29b9
FerEmilie <fernandezemilie@ymail.com>
2017-12-12 15:20:52 +0100

    test

--- routes/web.php
+++ routes/web.php
@@ -13,26 +13,26 @@
 
 //route ClientController
 Route::get('/', 'ClientController@index');
-Route::get('/liste', 'ClientController@liste');
-Route::get('/show/{id}', 'ClientController@show');
-Route::get('/create', 'ClientController@create');
-Route::post('/save', 'ClientController@save');
-Route::get('/edit/{id}', 'ClientController@edit');
-Route::post('/update/{id}', 'ClientController@update');
-Route::get('/delete/{id}', 'ClientController@delete');
-Route::get('/materiel', 'ClientController@ajoutmateriel');
-Route::post('/savemateriel', 'ClientController@savemateriel');
-Route::post('/search', 'ClientController@search');
+Route::get('liste', 'ClientController@liste');
+Route::get('show/{id}', 'ClientController@show');
+Route::get('create', 'ClientController@create');
+Route::post('save', 'ClientController@save');
+Route::get('edit/{id}', 'ClientController@edit');
+Route::post('update/{id}', 'ClientController@update');
+Route::get('delete/{id}', 'ClientController@delete');
+Route::get('materiel', 'ClientController@ajoutmateriel');
+Route::post('savemateriel', 'ClientController@savemateriel');
+Route::post('search', 'ClientController@search');
 //route ExcelController
 Route::get('importExport', 'ExcelController@importExport');
 Route::get('downloadExcel/{type}', 'ExcelController@downloadExcel');
 Route::post('importExcel', 'ExcelController@importExcel');
 //route MaterielController
-Route::get('/liste/materiel', 'MaterielController@liste');
-Route::get('/show/materiel/{id}', 'MaterielController@show');
-Route::post('/search/materiel', 'MaterielController@search');
+Route::get('liste/materiel', 'MaterielController@liste');
+Route::get('show/materiel/{id}', 'MaterielController@show');
+Route::post('search/materiel', 'MaterielController@search');
 //route StatController
-Route::get('/stats', 'StatController@index');
+Route::get('stats', 'StatController@index');
 //route authentification
 Auth::routes();
 //route admin voyager
